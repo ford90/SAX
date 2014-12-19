1fe5fc0bfc3ddd37f7ac6d0c081a58246dccbc3c
peter.ford <peter.ford@0738-99-100-HP.acmhq.acm.org>
2014-12-19 15:23:10 -0500

    Added Proceeding Model for Database

---
+++ src/proceeding/model/Proceeding.java
@@ -0,0 +1,236 @@
+package proceeding.model;
+
+public class Proceeding {
+
+	private String 	acronym;
+	private String	procDesc;
+	private String	procSubtitle;
+	private String 	procVolNo;
+	private String	conferenceNo;
+	private String	isbn;
+	private String	issn;
+	private String	cpYear;
+	private String	pubDate;
+	private String	pages;
+	private String	plusPages;
+	private String	orderNo;
+	private String	otherSource;
+	private String	confStartDt;
+	private String	confEndDt;
+	private String	confLocCity;
+	private String	confLocState;
+	private String	confUrl;
+	private String	fmText;
+	private String	fmFile;
+	private String	bmText;
+	private String	bmFile;
+	private String	isbn13;
+	private String	eissn;
+	private	String	coverImg;
+	private String	thumbImg;
+	private String	podEmail;
+	private String	podAddress;
+	private String	podDistributedBy;
+	private String	edCommentInt;
+	private String	edCommentPub;
+	private String	retinaImg;
+	private String	nonRetinaImg;
+	public String getAcronym() {
+		return acronym;
+	}
+	public void setAcronym(String acronym) {
+		this.acronym = acronym;
+	}
+	public String getProcDesc() {
+		return procDesc;
+	}
+	public void setProcDesc(String procDesc) {
+		this.procDesc = procDesc;
+	}
+	public String getProcSubtitle() {
+		return procSubtitle;
+	}
+	public void setProcSubtitle(String procSubtitle) {
+		this.procSubtitle = procSubtitle;
+	}
+	public String getProcVolNo() {
+		return procVolNo;
+	}
+	public void setProcVolNo(String procVolNo) {
+		this.procVolNo = procVolNo;
+	}
+	public String getConferenceNo() {
+		return conferenceNo;
+	}
+	public void setConferenceNo(String conferenceNo) {
+		this.conferenceNo = conferenceNo;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getIssn() {
+		return issn;
+	}
+	public void setIssn(String issn) {
+		this.issn = issn;
+	}
+	public String getCpYear() {
+		return cpYear;
+	}
+	public void setCpYear(String cpYear) {
+		this.cpYear = cpYear;
+	}
+	public String getPubDate() {
+		return pubDate;
+	}
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+	public String getPages() {
+		return pages;
+	}
+	public void setPages(String pages) {
+		this.pages = pages;
+	}
+	public String getPlusPages() {
+		return plusPages;
+	}
+	public void setPlusPages(String plusPages) {
+		this.plusPages = plusPages;
+	}
+	public String getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+	public String getOtherSource() {
+		return otherSource;
+	}
+	public void setOtherSource(String otherSource) {
+		this.otherSource = otherSource;
+	}
+	public String getConfStartDt() {
+		return confStartDt;
+	}
+	public void setConfStartDt(String confStartDt) {
+		this.confStartDt = confStartDt;
+	}
+	public String getConfEndDt() {
+		return confEndDt;
+	}
+	public void setConfEndDt(String confEndDt) {
+		this.confEndDt = confEndDt;
+	}
+	public String getConfLocCity() {
+		return confLocCity;
+	}
+	public void setConfLocCity(String confLocCity) {
+		this.confLocCity = confLocCity;
+	}
+	public String getConfLocState() {
+		return confLocState;
+	}
+	public void setConfLocState(String confLocState) {
+		this.confLocState = confLocState;
+	}
+	public String getConfUrl() {
+		return confUrl;
+	}
+	public void setConfUrl(String confUrl) {
+		this.confUrl = confUrl;
+	}
+	public String getFmText() {
+		return fmText;
+	}
+	public void setFmText(String fmText) {
+		this.fmText = fmText;
+	}
+	public String getFmFile() {
+		return fmFile;
+	}
+	public void setFmFile(String fmFile) {
+		this.fmFile = fmFile;
+	}
+	public String getBmText() {
+		return bmText;
+	}
+	public void setBmText(String bmText) {
+		this.bmText = bmText;
+	}
+	public String getBmFile() {
+		return bmFile;
+	}
+	public void setBmFile(String bmFile) {
+		this.bmFile = bmFile;
+	}
+	public String getIsbn13() {
+		return isbn13;
+	}
+	public void setIsbn13(String isbn13) {
+		this.isbn13 = isbn13;
+	}
+	public String getEissn() {
+		return eissn;
+	}
+	public void setEissn(String eissn) {
+		this.eissn = eissn;
+	}
+	public String getCoverImg() {
+		return coverImg;
+	}
+	public void setCoverImg(String coverImg) {
+		this.coverImg = coverImg;
+	}
+	public String getThumbImg() {
+		return thumbImg;
+	}
+	public void setThumbImg(String thumbImg) {
+		this.thumbImg = thumbImg;
+	}
+	public String getPodEmail() {
+		return podEmail;
+	}
+	public void setPodEmail(String podEmail) {
+		this.podEmail = podEmail;
+	}
+	public String getPodAddress() {
+		return podAddress;
+	}
+	public void setPodAddress(String podAddress) {
+		this.podAddress = podAddress;
+	}
+	public String getPodDistributedBy() {
+		return podDistributedBy;
+	}
+	public void setPodDistributedBy(String podDistributedBy) {
+		this.podDistributedBy = podDistributedBy;
+	}
+	public String getEdCommentInt() {
+		return edCommentInt;
+	}
+	public void setEdCommentInt(String edCommentInt) {
+		this.edCommentInt = edCommentInt;
+	}
+	public String getEdCommentPub() {
+		return edCommentPub;
+	}
+	public void setEdCommentPub(String edCommentPub) {
+		this.edCommentPub = edCommentPub;
+	}
+	public String getRetinaImg() {
+		return retinaImg;
+	}
+	public void setRetinaImg(String retinaImg) {
+		this.retinaImg = retinaImg;
+	}
+	public String getNonRetinaImg() {
+		return nonRetinaImg;
+	}
+	public void setNonRetinaImg(String nonRetinaImg) {
+		this.nonRetinaImg = nonRetinaImg;
+	}
+}
